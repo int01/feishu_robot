d3e0f7360fcffa1fc12612d031d0b5530a76365d
minhongwei <minhongwei@wiseasy.com>
2023-02-06 22:07:30 +0800

    增加了简单的连续性回到，

--- python/openaiApi.py
+++ python/openaiApi.py
@@ -1,32 +1,41 @@
+import logging
 import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from redisUtil import build_req_msg_txt, build_resp_msg_txt
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
 # https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/events/receive
 
 def snnd_openai_text(question_json_str, open_id):
     if request.method == "POST":
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(question_json_str, open_id),
-            temperature=0.7,
-            #     temperature min 0 ，max0.9
-            max_tokens=1024, # 2024
-            top_p=1,
-            stop=["Human:", "AI:"],  # ["wunike:","sage:"]  ["Human:", "AI:"]
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-        print("openAi api response text --->" + response.choices[0].text)
-        # return "问："+question_json_str+" \n答："+response.choices[0].text
-        return response.choices[0].text.lstrip('\n')
-    return "OPENAI PAI 接口没有返回内容，或请求参数错误。"
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=generate_prompt(question_json_str, open_id),
+                temperature=0.7,
+                #     temperature min 0 ，max0.9
+                max_tokens=1024,  # 2024
+                top_p=1,
+                stop=["Human:", "AI:"],  # ["wunike:","sage:"]  ["Human:", "AI:"]
+                frequency_penalty=0,
+                presence_penalty=0,
+            )
+            resp_text = response.choices[0].text.lstrip('\n')
+            print("openAi api response text --->" + resp_text)
+            build_resp_msg_txt(open_id, resp_text)
+            # return "问："+question_json_str+" \n答："+response.choices[0].text
+            return resp_text
+        except Exception as e:
+            logging.WARNING(e)
+            return "熬，你抓到了一个\"AI API\"接口异常。"
 
 
 def generate_prompt(question_json_str, open_id):
-    """ 对于连续性的提问 需要将聊天记录一起发送，用'\n\n'分隔每句话 TODO """
-    print("你说 --> " + question_json_str)
-    return question_json_str.capitalize()
+    req_text = build_req_msg_txt(open_id, question_json_str)
+    print("open_id 说 --> ", open_id, req_text)
+    return req_text.capitalize()
--- python/redisUtil.py
+++ python/redisUtil.py
@@ -1,9 +1,8 @@
 import redis
 import os
 
-
-host=os.getenv("REDIS_HOST")
-port=os.getenv("REDIS_PORT")
+host = os.getenv("REDIS_HOST")
+port = os.getenv("REDIS_PORT")
 
 pool = redis.ConnectionPool(host=host, port=port, decode_responses=True)
 r = redis.Redis(connection_pool=pool)
@@ -11,10 +10,11 @@ r = redis.Redis(connection_pool=pool)
 # r = redis.Redis(host=host, port=port, decode_responses=True)
 
 redis_msg_key = "MESSIGE_ID:"
+redis_msg_txt_key = "USER_OPENID:"
 
 
 def set_msg(msgId):
-    """默认过期时间15m"""
+    """过期时间15m"""
     r.set(redis_msg_key + msgId, msgId, ex=15 * 60)
 
 
@@ -35,3 +35,34 @@ def if_msg_value_repetition(msgId):
         return False
 
 
+def set_msg_txt(userOpenId, text):
+    """过期时间5m"""
+    r.set(redis_msg_txt_key + userOpenId, text, ex=30)
+
+
+def get_msg_txt(userOpenId):
+    return r.get(redis_msg_txt_key + userOpenId)
+
+
+def build_req_msg_txt(userOpenId, text):
+    """ 处理连续性的提问 需要将聊天记录一起发送，用'\n\n'分隔每句话 TODO """
+    key = redis_msg_txt_key + userOpenId
+    if r.exists(key):
+        r_text = get_msg_txt(userOpenId) + "\n\n" + text
+
+        # 临时的处理逻辑
+        if len(r_text) > 1000:
+            set_msg_txt(userOpenId, text)
+            return text
+
+        set_msg_txt(userOpenId, r_text)
+        return r_text
+    else:
+        set_msg_txt(userOpenId, text)
+        return text
+
+
+def build_resp_msg_txt(userOpenId, text):
+    """将ai的回复加到问句中"""
+    r_text = get_msg_txt(userOpenId) + "\n" + text
+    set_msg_txt(userOpenId, r_text)
